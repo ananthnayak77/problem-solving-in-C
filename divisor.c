1c231bf5678ed285d38ef8b8fbe15d6fd2ac18f3
ananthnayak77 <ananthnayakmuniyal@gmail.com>
2026-09-16 20:55:40 +0530

    divisor sum replacer

---
+++ divisor.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{int a[10],i,n,sum=0;
+printf("enter number of elements :");
+scanf("%d",&n);
+printf("enter the elements=");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+a[i]%i==0;
+sum=sum+i;
+if(a[i]%i==0 && sum>a[i])
+{printf("output is %d",sum);}
+else
+{printf("no such element foundis -1");}
+return 0;
+}
