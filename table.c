2abe5df2feedfd38dcf5e4f072e81153690194be
ananthnayak77 <ananthnayakmuniyal@gmail.com>
2026-09-24 18:24:13 +0530

    to find the symmetric and not symmetric matrix

--- table.c
+++ table.c
@@ -30,7 +30,7 @@ int main() {
     
     printf("\nSTUDENTS_MARK (Roll No and Marks):\n");
     for(i = 0; i < N; i++) {
-        printf("%d and the %d\n", STUDENTS_MARK[0][i], STUDENTS_MARK[1][i]);
+        printf("%d and  %d\n", STUDENTS_MARK[0][i], STUDENTS_MARK[1][i]);
     }
     return 0;
 }
